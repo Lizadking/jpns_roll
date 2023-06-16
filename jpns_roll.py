2449415dd98c5557a5cbea74dfaeb7b997e4598d
Lizadking <torrencebarbourjr04@gmail.com>
2023-06-16 00:17:35 -0400

    embdeds now work

--- jpns_roll.py
+++ jpns_roll.py
@@ -24,15 +24,37 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 
-
-        
-
-
+"""
+function: on_read()
+pre-condition: none
+post-condition: initalized instance of the bot
+comments: 
+"""
 @client.event
 async def on_ready():
-    load_dotenv()
+
+    #get the sync up ready for the slash commands 
+    try:
+        synced = await client.tree.sync()
+        print(f"synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
     
     print(f"ready to ROLL")
 
+
+"""
+function: help()
+pre-condition: none
+post-condition:displays help message
+comments:  emphemeral -> the message only appears to the user 
+"""
+@client.tree.command(name="help")
+async def help(interaction:discord.Interaction):
+        #create the embed 
+        not_done_yet_file = discord.File("wheres_that_feature.png",filename= "wheres_that_feature.png")
+        message_embed =  discord.Embed()
+        message_embed.set_image(url="attachment://wheres_that_feature.png")
+        await interaction.response.send_message(file=not_done_yet_file,embed=message_embed,ephemeral= True)
 
 client.run(TOKEN)
